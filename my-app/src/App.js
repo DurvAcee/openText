691bd684268bce48f8d9690803060f7653d8028f
Durvesh Danve <durvesh.danve@gmail.com>
2022-12-27 17:51:27 +0530

    Added my-app repo

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import Navbar from './components/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Navbar title="TextUtils" abtText="About Us"/>
+    </>
+  );
+}
+
+export default App;
