1f430fbef733c36fb83f6bd6782f57da5484ccaf
Durvesh Danve <durvesh.danve@gmail.com>
2022-12-28 12:59:37 +0530

    Added more functions with state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,9 +5,9 @@ import './App.css';
 function App() {
   return (
     <>
-      <Navbar title="TextUtils" abtText="About Us"/>
+      <Navbar title="Text Analyzer" abtText="About Us"/>
       <div className="container my-3">
-          <TextForm heading="Enter The Text to Analyze Below"/>
+          <TextForm heading="Enter The Text :"/>
       </div>
     </>
   );
--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react'
 
-
-
 export default function TextForm(props) {
     const [text, setText] = useState('');
 
     const handleUpperCase = () =>{
+        emptyTextFound();
         setText(text.toUpperCase());
     }
 
@@ -14,7 +13,27 @@ export default function TextForm(props) {
     }
 
     const handleLowerCase = () =>{
+        emptyTextFound();
         setText(text.toLowerCase());
+    }
+
+    const handleClearText = () => {
+        setText("");
+    }
+
+    const emptyTextFound = () => {
+        if(!text){
+            alert('Please Enter some Text');
+        }
+}
+
+    const handleCopyClipboard = () => {
+        navigator.clipboard.writeText(text).then(function(){
+            let result = text ? 'text copied to clipboard!' : 'Please Enter some Text!';
+            alert(result);
+        }, function(err){
+            alert('Failed: Could not copy text: ',err);
+        });
     }
   
     return (
@@ -24,8 +43,10 @@ export default function TextForm(props) {
             <div class="mb-3">
                 <textarea className="form-control" id="myTextBox" value={text} onChange={handleOnChange} rows="8"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpperCase}>Convert to Upper Case</button>
-            <button className="btn btn-primary mx-2" onClick={handleLowerCase}>Convert to Lower Case</button>
+            <button className="btn btn-primary mx-1" onClick={handleUpperCase}>Convert to Upper Case</button>
+            <button className="btn btn-primary mx-1" onClick={handleLowerCase}>Convert to Lower Case</button>
+            <button className="btn btn-danger mx-1" onClick={handleClearText}>Clear Text</button>
+            <button className="btn btn-success mx-1" onClick={handleCopyClipboard}>Copy to Clipboard</button>
         </div>
 
         <div className="container my-3">
