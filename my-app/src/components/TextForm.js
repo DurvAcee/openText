f9f1e7d429dbf6047aad28a211e56be49090442f
Durvesh Danve <durvesh.danve@gmail.com>
2022-12-28 12:22:54 +0530

    Added more functionality to TextForm

--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -3,23 +3,38 @@ import React, { useState } from 'react'
 
 
 export default function TextForm(props) {
-    const [text, setText] = useState('Enter text here');
+    const [text, setText] = useState('');
 
-    const handleOnClick = () =>{
+    const handleUpperCase = () =>{
         setText(text.toUpperCase());
     }
 
     const handleOnChange = (event) =>{
         setText(event.target.value);
     }
+
+    const handleLowerCase = () =>{
+        setText(text.toLowerCase());
+    }
   
     return (
-        <div>
+        <>
+        <div className="container">
             <h2>{props.heading}</h2>
             <div class="mb-3">
                 <textarea className="form-control" id="myTextBox" value={text} onChange={handleOnChange} rows="8"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleOnClick}>Convert to UpperCase</button>
+            <button className="btn btn-primary" onClick={handleUpperCase}>Convert to Upper Case</button>
+            <button className="btn btn-primary mx-2" onClick={handleLowerCase}>Convert to Lower Case</button>
         </div>
+
+        <div className="container my-3">
+            <h2> Your Text Summary</h2>
+            <p> {text.split(" ").length} Words, {text.length} Characters</p>
+            <p> {0.008 * text.split(" ").length} Minutes to Read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+        </>
     )
 }
